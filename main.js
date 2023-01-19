177097c9c13d406ef72f6d23de88ecdbf87ff0f8
Becky Rushton <beckyrushton30@gmail.com>
2023-01-19 17:10:42 +0000

    Added js that inputs text from button to window

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 console.log("hello world");
 
-// const button = document.querySelectorAll("calculator__button");
+// const buttonOne = document.getElementById("calculator__button--one");
+// console.log(buttonOne);
+// const windowCalc = document.getElementById("calcwindow");
 
 // const handleClick = () => {
-//   button.innerText = innerText[i];
+//   windowCalc.innerText = buttonOne.innerText;
+//   console.log("clicked");
 // };
 
-// button.forEach((button) => {
-//   button.addEventListener = event.target.value;
-// });
+// buttonOne.addEventListener("click", handleClick);
+
+const inputButtons = document.querySelectorAll(".calculator__input");
+console.log(inputButtons);
+const windowCalc = document.getElementById("calcwindow");
+
+const handleClick = (inputButton) => {
+  windowCalc.innerText += inputButton.innerText;
+  console.log("click");
+};
+
+inputButtons.forEach((inputButton) => {
+  inputButton.addEventListener("click", () => handleClick(inputButton));
+});
