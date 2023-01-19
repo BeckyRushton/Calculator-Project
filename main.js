c9587d626b0be02bde02cb14bd0d85b987d8e672
Becky Rushton <beckyrushton30@gmail.com>
2023-01-19 15:58:12 +0000

    added box shadows to buttons

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 console.log("hello world");
+
+// const button = document.querySelectorAll("calculator__button");
+
+// const handleClick = () => {
+//   button.innerText = innerText[i];
+// };
+
+// button.forEach((button) => {
+//   button.addEventListener = event.target.value;
+// });
