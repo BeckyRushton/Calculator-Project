08d9d38bcdc521b6eda8461a8cd059873a9b0643
Becky Rushton <beckyrushton30@gmail.com>
2023-01-24 14:40:29 +0000

    Added all comments to explain js methodology

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const equalsButton = document.getElementById("calculator__button--equals");
 // method is a global variable as it is interchangable in the handleMethod and handleEquals functions
 let method;
 
-// Functions
+// Functions - creating functions for the calculator to process inputs
 
 // handleClick handles the input buttons pressed on the calculator pad to show whatever is
 // in the innerText in the calculator window
@@ -49,12 +49,22 @@ const handleBack = () => {
   }
 };
 
+// handleMethod looks at the method buttons (+, -, / and *) and calls them an event individually
+// numberOne is declared as the first input in the calc window
+// method is the chosen method of the sum and takes the innerText of that input method
+// This is then applied to the numberOne as a single entity (via +=) to be used later
 const handleMethod = (event) => {
   numberOne = windowCalc.innerText;
   method = event.target.innerText;
   windowCalc.innerText += method;
 };
 
+// handleEquals takes the string we have entered into the calc window and splits it by the method
+// Then number0ne and numberTwo are converted into number types (float allows decimals) as an array
+// to be used in the conditional statement section next
+// if and else if statements look for which method has been input to run the corresponding sum and
+// replace the sum with the answer on the calc window
+// else is a default in place if a method has not been input to avoid error messages
 const handleEquals = () => {
   const sumString = windowCalc.innerText.split(method);
   numberOne = parseFloat(sumString[0]);
@@ -71,22 +81,27 @@ const handleEquals = () => {
   } else {
     windowCalc.innerText = 0;
   }
-
+  // method is outside of the conditional statements as it has to be changed for each statement
   method = "";
 };
 
-// Interactions
+// Interactions for the calculator to carry out the functions
 
+// This allows us to click on any of the input buttons so we can see it on the calc window
 inputButtons.forEach((inputButton) => {
   inputButton.addEventListener("click", () => handleClick(inputButton));
 });
 
+// This is a simple function that runs once upon a click
 allClear.addEventListener("click", handleClear);
 
+// This is a simple function that runs once upon a click as well
 backButton.addEventListener("click", handleBack);
 
+// This allows us to click on any of the method buttons so we can use it in our sum
 methodButtons.forEach((methodButton) => {
   methodButton.addEventListener("click", handleMethod);
 });
 
+// This allows us to run the sum we have input into the calc window and see the result
 equalsButton.addEventListener("click", handleEquals);
